1e75b19cea5993ce0de9071d05e8d662edcfc4da
Bar Horing Amir <bar.apple@gmail.com>
2022-04-27 12:13:09 +0300

    custom reducer

--- src/useFetch.js
+++ src/useFetch.js
@@ -1,27 +1,49 @@
-import { useState, useEffect } from 'react';
+import { useEffect, useReducer } from 'react';
+
+const LOADING = 'LOADING';
+const RESPONSE_COMPLETE = 'RESPONSE_COMPLETE';
+const ERROR = 'ERROR';
+
+const initialState = {
+  result: null,
+  loading: true,
+  error: null,
+};
+
+const fetchReducer = (state, action) => {
+  if (action.type === LOADING) {
+    return initialState;
+  }
+  if (action.type === RESPONSE_COMPLETE) {
+    return { result: action.payload, error: null, loading: false };
+  }
+  if (action.type === ERROR) {
+    return {
+      loading: false,
+      result: null,
+      error: action.payload.error,
+    };
+  }
+
+  return state;
+};
 
 export const useFetch = (url) => {
-  const [response, setResponse] = useState(null);
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState(null);
-
+  const [state, dispatch] = useReducer(fetchReducer, initialState);
   useEffect(() => {
-    // setLoading(true);
-    // setResponse(null);
-    // setError(null);
-
+    dispatch({ type: LOADING });
     fetch(url)
       .then((response) => response.json())
       .then((response) => {
-        setLoading(false);
-        setError(false);
-        setResponse(response);
+        dispatch({ type: RESPONSE_COMPLETE, payload: response });
       })
       .catch((error) => {
-        setLoading(false);
-        setError(error.message);
+        console.log('error is', error.message);
+        dispatch({ type: ERROR, payload: { error } });
       });
   }, []);
+
+  const { result: response, loading, error } = state;
 
   return [response, loading, error];
 };
